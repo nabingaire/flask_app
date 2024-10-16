a42e6913b0220a2131b02561bb69a3564e6b89ea
Nabin Gaire <gairenabin2053@gmail.com>
2024-10-15 21:25:45 -0400

    final commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,19 +1,22 @@
 from flask import Flask,render_template
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 app = Flask(__name__)
 
+client = MongoClient("mongodb+srv://nabin:nabin@shop.sccvu.mongodb.net/?retryWrites=true&w=majority&appName=shop",server_api=ServerApi('1'))
+db = client.get_database('shop_db')
+products_all = db.get_collection('products')
 
-client = MongoClient("mongodb+srv://nabin:nabin@shop.sccvu.mongodb.net/?retryWrites=true&w=majority&appName=shop")
-db = client.app
-products_all = db.products
 @app.route('/')
-def home():  # put application's code here
+def home():  # put application's code
     return render_template('home.html')
 
 @app.route('/products')
 def products():
-    products = list(products_all.find())# put application's code here
-    return render_template('products.html', products=products)
+        products = list(products_all.find())  # put application's code here
+        return render_template('products.html', products=products)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    app.run()
